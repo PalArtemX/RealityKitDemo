cbb603982f5ffb011bb16f2ac28d854fb24b6e89
Artem Paliutin <pal.artem@icloud.com>
2022-10-27 13:52:35 +0300

    added RemoveAll

--- TestForMixar/ViewControllers/ARViewController.swift
+++ TestForMixar/ViewControllers/ARViewController.swift
@@ -16,9 +16,18 @@ class ARViewController: UIViewController {
     let addBoxButton: UIButton = {
         let button = UIButton(configuration: .borderedTinted())
         button.configuration?.image = UIImage(systemName: "plus.square.dashed")
-        button.configuration?.title = "Add Box"
+        button.configuration?.title = "Box"
         button.tintColor = .green
         button.addTarget(.none, action: #selector(addBox), for: .touchUpInside)
+        return button
+    }()
+    
+    let removeAllBoxes: UIButton = {
+        let button = UIButton(configuration: .borderedTinted())
+        button.configuration?.image = UIImage(systemName: "trash.square")
+        button.configuration?.title = "All"
+        button.tintColor = .red
+        button.addTarget(.none, action: #selector(removeAllAnchors), for: .touchUpInside)
         return button
     }()
 
@@ -28,9 +37,7 @@ class ARViewController: UIViewController {
         arView = ARView(frame: view.bounds)
         view.addSubview(arView)
         
-        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapSwitchColorBox)))
-        arView.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handleTapRemoveBox)))
-        
+        addGestureRecognizers()
         addSubviewForARView()
         setupConstraints()
         
@@ -40,16 +47,26 @@ class ARViewController: UIViewController {
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             addBoxButton.centerXAnchor.constraint(equalTo: arView.centerXAnchor),
-            addBoxButton.bottomAnchor.constraint(equalTo: arView.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+            addBoxButton.bottomAnchor.constraint(equalTo: arView.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            
+            removeAllBoxes.leadingAnchor.constraint(equalTo: addBoxButton.trailingAnchor, constant: 10),
+            removeAllBoxes.bottomAnchor.constraint(equalTo: arView.safeAreaLayoutGuide.bottomAnchor, constant: -10)
         ])
     }
     
     private func addSubviewForARView() {
-        let subviews = [addBoxButton]
+        let subviews = [addBoxButton, removeAllBoxes]
         subviews.forEach { subview in
             subview.translatesAutoresizingMaskIntoConstraints = false
             arView.addSubview(subview)
         }
+    }
+    
+    
+    // MARK: - Add Gesture Recognizers
+    private func addGestureRecognizers() {
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapSwitchColorBox)))
+        arView.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(handleTapRemoveBox)))
     }
     
     
@@ -79,7 +96,7 @@ class ARViewController: UIViewController {
         let anchor = AnchorEntity(plane: .horizontal)
         let material = SimpleMaterial(color: .systemRed, isMetallic: true)
         
-        let box = ModelEntity(mesh: .generateBox(size: 0.2), materials: [material])
+        let box = ModelEntity(mesh: .generateBox(size: 0.1), materials: [material])
         box.generateCollisionShapes(recursive: true)
         box.transform = Transform(pitch: 0, yaw: 1, roll: 0)
         
@@ -87,6 +104,10 @@ class ARViewController: UIViewController {
         
         anchor.addChild(box)
         arView.scene.addAnchor(anchor)
+    }
+    
+    @objc func removeAllAnchors() {
+        arView.scene.anchors.removeAll()
     }
     
     
